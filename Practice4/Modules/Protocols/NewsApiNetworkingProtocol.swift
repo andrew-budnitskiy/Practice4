d7aa7e3d847c30139e81dd9950d4a4d3731445f7
Andrey Budnitskiy <mrAndrew@MacBook-Pro-Andrey.local>
2023-01-15 00:06:31 +0500

    15.01_1

--- Practice4/Modules/Protocols/NewsApiNetworkingProtocol.swift
+++ Practice4/Modules/Protocols/NewsApiNetworkingProtocol.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-
+import Combine
 
 typealias NewsApiSourcesResult = (Result<NewsApiSources?, Error>) -> Void
 typealias NewsApiResultsResult = (Result<NewsApiResults?, Error>) -> Void
